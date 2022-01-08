369f797eed4284d638d38b9b5dcd76e21a535a0b
unknown <Your email>
2022-01-08 12:01:27 +0600

    initial commit

--- main/views.py
+++ main/views.py
@@ -13,21 +13,33 @@ def get_data(request):
     }
     return Response(data=context)
 
-@api_view(["GET"])
+@api_view(["GET", 'POST'])
 def movie_list_view(request):
-    movies = Movie.objects.all()
-    data = MovieListSerializer(movies, many=True).data
-    return Response(data=data)
+    if request.method == 'GET':
+        movies = Movie.objects.all()
+        data = MovieListSerializer(movies, many=True).data
+        return Response(data=data)
+    elif request.method == 'POST':
+        print(request.data)
+        title = request.data['title']
+        description = request.data['description']
+        genre_id = request.data['genre_id']
+        movies = Movie.objects.create(
+            title=title,
+            description=description,
+            genre_id=genre_id
+        )
+        return Response(data={'message': 'OK'})
 
 @api_view(['GET'])
 def movie_detail_view(request, id):
-    try:
-        movie = Movie.objects.get(id=id)
-    except Movie.DoesNotExist:
-        return Response(data={'message': 'Product not found'},
-                        status=status.HTTP_404_NOT_FOUND)
-    data = MovieListSerializer(movie).data
-    return Response(data=data)
+            try:
+                movie = Movie.objects.get(id=id)
+            except Movie.DoesNotExist:
+                return Response(data={'message': 'Product not found'},
+                                status=status.HTTP_404_NOT_FOUND)
+            data = MovieListSerializer(movie).data
+            return Response(data=data)
 
 @api_view(['GET'])
 def genres_view(request):
